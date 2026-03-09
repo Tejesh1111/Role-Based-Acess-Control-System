dfffffcecf29cc3b63c7f7c4c86c038131a94176
Tejesh1111 <gaddetejesh@gmail.com>
2026-03-09 19:14:18 +0530

    Added frontend change for deployment

--- Frontend/src/api/axios.ts
+++ Frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://role-based-acess-control-system.onrender.com",
 });
 
 api.interceptors.response.use(

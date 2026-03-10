5bc7487ecb82bbeb16c93cfa9d6cdf5d87f374ca
Tejesh1111 <gaddetejesh@gmail.com>
2026-03-10 12:11:48 +0530

    Changed Decker File

--- Frontend/src/api/axios.ts
+++ Frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://role-based-acess-control-system.onrender.com",
+  baseURL: "https://rbac-backend.up.railway.app",
 });
 
 api.interceptors.response.use(
